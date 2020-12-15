b5f22faaaa0885a6da18bbe408210722aa535c3f
lironcohen8 <lironhen12123@gmail.com>
2020-12-15 14:04:53 +0200

    measurements trying

--- src/Tests/Measurments.java
+++ src/Tests/Measurments.java
@@ -5,10 +5,11 @@ import java.util.Random;
 
 public class Measurments {
 
-	public void insertionSort(int arr[]) 
+	public static long insertionSort(int arr[]) 
     { 
+		long count = 0;
         int n = arr.length; 
-        for (int i = 1; i < n; ++i) { 
+        for (int i = 1; i < n; i++) { 
             int key = arr[i]; 
             int j = i - 1; 
   
@@ -17,29 +18,56 @@ public class Measurments {
                of their current position */
             while (j >= 0 && arr[j] > key) { 
                 arr[j + 1] = arr[j]; 
+                count++;
                 j = j - 1; 
             } 
             arr[j + 1] = key; 
-        } 
+        }
+        return count;
     }
 	
-	public static int[] rand(int n) {
-		Random random = new Random();
+	public static int[] down(int n) {
 		int[] res = new int[n];
-		for (int i=0; i<n; i++) {
-			int val = random.nextInt(n);
-			res[i] = val;
-			}
+		for (int i = 0 ; i<n; i++)
+			res[i] = n-i;
 		return res;
 	}
 	
-
+	public static int[] rand(int[] arr) {
+		Random random = new Random();
+//		int[] res = new int[n];
+		for (int i=0; i<arr.length; i++) {
+			int val = random.nextInt(arr.length);
+			arr[i] = val;
+			}
+		return arr;
+	}
+	
+	static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+	
 public static void main(String[] args)
 {
-	for (int i=1; i<11; i++) {
-		int[] randArr = new int[100*i];
-		randArr = rand(10000*i);
-		System.out.println(Arrays.toString(randArr));
-	}
+	//for (int i=1; i<11; i++) {
+		int i = 1;
+		int[] randArr = new int[10*i];
+		randArr = rand(randArr);
+		System.out.println("before sorting: ");
+		printArray(randArr);
+		System.out.println("rand " + 10000*i + " count: " + insertionSort(randArr));
+		System.out.println("after sorting: ");
+		printArray(randArr);
+
+		int[] downArr = new int[10000*i];
+		downArr = down(10000*i);
+		System.out.println("before sorting: " + Arrays.toString(downArr));
+		System.out.println("down " + 10000*i + " count: " + insertionSort(downArr));
+		System.out.println("after sorting: " + Arrays.toString(downArr));
+	//}
 }
 }
